88c88485ee05347c7a6b1a2386f086204bde5305
estevo <pedro.cordova2002@gmail.com>
2023-08-30 22:53:44 -0300

    updating path clientes post

--- travel-cli/src/main/java/org/estevao/cliente/ClienteService.java
+++ travel-cli/src/main/java/org/estevao/cliente/ClienteService.java
@@ -4,7 +4,6 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "http://localhost:8090/clientes")
